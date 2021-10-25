7b307fee80b0e2f902a82b8ebd96ec6c7fd6d5fb
Matthew Denune <mattdenune@gmail.com>
2021-10-25 16:46:18 -0400

    cleaned functions, added props to List.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import List from './components/List';
 
 function App() {
 
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = event => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="App">
       <h1> My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input id='search' type="text" />
+      <input id='search' type="text" onChange={handleChange} />
 
       <hr />
 
-     <List />
+     <List list={stories} />
     </div>
   );
 }
