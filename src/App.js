48224c4f1e745f7ef2fc64b9ec2505a216744476
Matthew Denune <mattdenune@gmail.com>
2021-10-25 16:09:13 -0400

    created List.js and moved map function into it.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
+import List from './components/List';
 
 function App() {
 
@@ -30,18 +12,7 @@ function App() {
 
       <hr />
 
-      {list.map(item => {
-        return (
-          <div key ={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )
-      })}
+     <List />
     </div>
   );
 }
