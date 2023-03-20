f2766f469b55bdc50c1ce9b619f6c991e2e66e9b
Matthew Denune <mattdenune@gmail.com>
2023-03-20 16:24:18 -0400

    added sorting to List

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,61 @@
 import React from 'react';
+import { sortBy } from "lodash";
 
 import Item from './Item';
 
 
-const List = React.memo( ({ list, onRemoveItem }) =>
-	console.log('B: List') || 
-  <div>
-    <div style={{ display: 'flex' }}>
-      <span style={{ width: '40%' }}>Title</span>
-      <span style={{ width: '30%' }}>Author</span>
-      <span style={{ width: '10%' }}>Comments</span>
-      <span style={{ width: '10%' }}>Points</span>
-      <span style={{ width: '10%' }}>Actions</span>
-    </div>
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'author'),
+  COMMENT: list => sortBy(list, 'num_comments').reverse(),
+  POINT: list => sortBy(list, 'points').reverse(),
+};
 
-    {list.map(item => (
-      <Item
-        key={item.objectID}
-        item={item}
-        onRemoveItem={onRemoveItem}
-      />
-    ))}
-  </div>
-);
+
+const List = React.memo( ({ list, onRemoveItem }) => {
+	const [sort, setSort] = React.useState('NONE');
+
+	const handleSort = sortKey => {
+		setSort(sortKey);
+	};
+
+	const sortFuntion = SORTS[sort];
+	const sortedList = sortFuntion(list);
+
+	return (
+    console.log("B: List") || (
+      <div>
+        <div style={{ display: "flex" }}>
+          <span style={{ width: "40%" }}>
+            <button type="button" onClick={() => handleSort("TITLE")}>
+              Title
+            </button>
+          </span>
+          <span style={{ width: "30%" }}>
+            <button type="button" onClick={() => handleSort("AUTHOR")}>
+              Author
+            </button>
+          </span>
+          <span style={{ width: "10%" }}>
+            <button type="button" onClick={() => handleSort("COMMENT")}>
+              Comments
+            </button>
+          </span>
+          <span style={{ width: "10%" }}>
+            <button type="button" onClick={() => handleSort("POINT")}>
+              Points
+            </button>
+          </span>
+          <span style={{ width: "10%" }}>Actions</span>
+        </div>
+
+        {sortedList.map((item) => (
+          <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+        ))}
+      </div>
+    )
+  );
+});
+
 export default List;
